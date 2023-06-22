a0daf0a3990a4431a317778c2fa9852e7edd9fd1
hastaelsuel0 <130052949+hastaelsuel0@users.noreply.github.com>
2023-06-22 03:23:37 +0200

    Add files via upload

---
+++ player/static/load.js
@@ -0,0 +1,18 @@
+const argz = new URLSearchParams(window.location.search);
+
+const url = argz.get("u");
+const postImage = argz.get("i");
+const captions_file = argz.get("c");
+
+let token;
+
+fetch("https://back.laisla.pm/api/key")
+  .then((response) => response.json())
+  .then((data) => adios(data["key"]));
+
+const adios = function (token) {
+  const hlsUrl = url + "?" + token;
+  window.location.assign(
+    "player.html?u=" + hlsUrl + "&i=" + postImage + "&c=" + captions_file
+  );
+};
