38f48e112a14a960a7016532271eedf2fc8e9f1b
hastaelsuel0 <130052949+hastaelsuel0@users.noreply.github.com>
2024-02-22 01:37:24 +0100

    Update load.js

--- player/static/load.js
+++ player/static/load.js
@@ -6,22 +6,6 @@ const captions_file = argz.get("c");
 
 let token;
 
-async function detectAdBlock() {
-  let adBlockEnabled = false
-  const googleAdUrl = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-  try {
-    await fetch(googleAdUrl).catch(_ => adBlockEnabled = true)
-  } catch (e) {
-    adBlockEnabled = true
-  } finally {
-    return adBlockEnabled;
-  }
-}
-
-if (detectAdBlock()) {
-  alert("Desactiva tu AdBlock y vuelve a entrar");
-  return;
-};
 
 
 fetch("https://back.laisla.pm/api/key")
