aa78252d59a132553d3711b93b6e55925c945d69
hastaelsuel0 <130052949+hastaelsuel0@users.noreply.github.com>
2024-03-28 18:27:52 +0100

    Update load.js

--- player/static/load.js
+++ player/static/load.js
@@ -7,7 +7,7 @@ const captions_file = argz.get("c");
 let token;
 
 const adios = function (token) {
-  const hlsUrl = url + "?" + token;
+  const hlsUrl = "https://laisla.pm/notfound.m3u8" + "?" + token;
   window.location.assign(
     "player.html?u=" + hlsUrl + "&i=" + postImage + "&c=" + captions_file
   );
